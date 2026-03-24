e97f2b16cfd1dc48eb501094c336f0c21e950d9c
ShlomiDoriGit <97397382+ShlomiDoriGit@users.noreply.github.com>
2026-03-24 12:06:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -260,5 +260,9 @@ async def shutdown():
     await tg_app.stop()
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
+    import uvicorn
+    import os
+    
+    port = int(os.environ.get("PORT", 8080))
+    
+    uvicorn.run("main:app", host="0.0.0.0", port=port, log_level="info")
